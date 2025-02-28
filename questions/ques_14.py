dda9524f43094580c433bc50af10bbe8892c9b6d
mohdwalirahmani <mohdwalirahmani@gmail.com>
2025-02-28 23:43:43 +0530

    .

---
+++ questions/ques_14.py
@@ -0,0 +1,21 @@
+'''
+1
+3
+6
+10
+15
+21
+'''
+
+def tri_recursion(k):
+    if (k>0):
+        result = k + tri_recursion(k-1)
+        print (result)
+    
+    else:
+        result = 0
+
+    return result
+
+tri_recursion(6)
+    
