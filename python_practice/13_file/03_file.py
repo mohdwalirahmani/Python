581ec67d47458a90ce35e132b1a9b4f5f0a667f3
mohdwalirahmani <mohdwalirahmani@gmail.com>
2025-03-15 02:36:49 +0530

    .

--- python_practice/13_file/03_file.py
+++ python_practice/13_file/03_file.py
@@ -16,3 +16,16 @@ while (lines != ""):
     line = f.readline()
 
 f.close()
+
+# y loop m chale ja rha h, idk kese shi hoga
+
+'''
+modes-
+r - to read the content from file
+w - to write the content into file
+a - to append the existing content into file
+r+:  To read and write data into the file. The previous data in the file will be overridden.
+w+: To write and read data. It will override existing data.
+a+: To append and read data from the file. It won’t override existing data.
+
+'''
