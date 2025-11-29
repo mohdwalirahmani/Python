e7ee7f8a5712ac4d8bd3ff1d301efca0853f1f42
Mohd Wali Rahmani <mohdwalirahmani@gmail.com>
2025-11-29 21:24:08 +0530

    add

---
+++ python_practice/18_import.py
@@ -0,0 +1,16 @@
+import math
+import random as r
+import datetime
+# from recursion import *
+
+dt = datetime.datetime.now()
+print(f"date and time is {dt}")
+
+print(r.randint(1,11))
+print(r.choice(["Apple", "mango", "banana"]))
+
+print(math.sqrt(550))
+# print(dir(math))
+
+# n = int(input("Enter the number: "))
+# print("the factorial of the number is: ",factorial(n))
