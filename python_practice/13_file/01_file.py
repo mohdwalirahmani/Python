1dcae91ef51d53850ca3eced7ff6380d1b14a337
Mohd Wali Rahmani <mohdwalirahmani@gmail.com>
2025-12-01 14:04:38 +0530

    upd

--- python_practice/13_file/01_file.py
+++ python_practice/13_file/01_file.py
@@ -6,3 +6,5 @@ f.close()
 q = open ("04_file.text", "w") # this file doesn't exists initially but if we write this code it will automatically create a file
 data = q.read()
 print (q)
+
+# we can use "x" to create a file and it will gives us error if that file exists already
--- python_practice/13_file/02_file.py
+++ python_practice/13_file/02_file.py
@@ -1,8 +1,13 @@
 str = "hey, my name is Mohd Wali"
 
-f = open("02_file.txt", "w")
+f = open("02_file.txt", "w")    # y sirf likhega aur agr baar baar ise run krenge to overwrite kr dega aur sirf ek hi baar write hoga
 
+# f = open("02_file.txt", "a")    # y append kr dega, jitni baar chalaoge utni baar write krta jayega
 
 f.write(str)
-
 f.close()
+
+with open("02_file.txt", "a") as f:
+    f.write("\nNow I'm using 'with'")
+
+# using 'with' we don't have to close the file
